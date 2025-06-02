164b0e6b3bb73b0f46a3fe24d93c27f3e2b69b42
napoly <napolytan@protonmail.com>
2025-06-02 11:59:05 +0200

    Add Playwright integration test

---
+++ IntegrationTest/PlaywrightPluginIntegrationTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright;
+using Xunit;
+
+namespace IntegrationTest;
+
+public class PlaywrightPluginIntegrationTest : IAsyncLifetime
+{
+    private IPage _page;
+    private IBrowser _browser;
+
+    public async Task InitializeAsync()
+    {
+        var playwright = await Playwright.CreateAsync();
+        _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+        var context = await _browser.NewContextAsync();
+        _page = await context.NewPageAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _browser.CloseAsync();
+    }
+    
+    [Fact]
+    public async Task ExecutePluginSuccessfully()
+    {
+        await _page.GotoAsync("http://localhost:5000/");
+        await _page.ClickAsync("a[href='/myplugin']");
+        var textContent = await _page.InnerTextAsync("p");
+        Assert.Equal("Hello world from Plugin!", textContent);
+    }
+}
